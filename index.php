b41c0ab146cb3152a72ef8c759511f20b9f1ec98
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2021-10-20 15:14:40 -0700

    Added our php file !# On branch main

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+	
+	<head><meta charset="utf-8">
+	<title>Example 2</title>
+	</head>
+	
+	<body>
+	    <p>Hello</p>
+	    
+	    <form method="get" action="https://google.com/search">
+	    	<label>Search keywords: <input type="text" name="q"></label>
+	    	<input type="submit">
+	    	<input type="reset">
+	    </form>
+	    
+	    	
+	</body>
+
+</html>
