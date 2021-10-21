03f7efba23eabaf0221ef6637c86e143f8bcf5e3
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2021-10-20 17:05:02 -0700

    idk

--- index.php
+++ index.php
@@ -8,6 +8,34 @@
 	
 	<body>
 	    <p>Hello</p>
+
+	    <?php
+
+			$dbuser = "root";
+			$dbpass = "wutang2218";
+
+			$db = mysqli_connect("localhost", $dbuser, $dbpass, "refbookdb");
+
+			if ( ! $db ) // connection failed
+			{
+				print "<p>Could not connect to database</p>";
+				print ( mysqli_connect_error() );
+				print "</body></html>";
+				mysqli_close($db);
+				die();	// go no further than this line!
+			}
+			else
+			{
+				print "<p>Connection succeeded</p>";
+				//	var_dump($db);
+
+				/*
+					this is also a comment
+				*/
+	
+				error_log("Hi I am an error and db username is " . $dbuser . "\r\n");			
+	    }
+	?>
 	    
 	    <form method="get" action="https://google.com/search">
 	    	<label>Search keywords: <input type="text" name="q"></label>
